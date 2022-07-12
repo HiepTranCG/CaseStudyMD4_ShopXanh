f42217640b8d147a5295ffc335272ca91d49cc7d
TranHuyHiep <hiep.tran@codegym.vn>
2022-07-12 09:34:51 +0700

    first commit

---
+++ src/main/java/com/example/casestudymd4_shopxanh/CaseStudyMd4ShopXanhApplication.java
@@ -0,0 +1,13 @@
+package com.example.casestudymd4_shopxanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyMd4ShopXanhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyMd4ShopXanhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casestudymd4_shopxanh/CaseStudyMd4ShopXanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casestudymd4_shopxanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyMd4ShopXanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
